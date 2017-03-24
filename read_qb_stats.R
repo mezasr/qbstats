505139dd2bc0d6720bce15e9101976d5d67b2724
steve <mezasr@gmail.com>
2017-03-24 00:23:32 -0400

    example of sorting yards by qb age/year
    using dplyr pipelining in R.

--- read_qb_stats.R
+++ read_qb_stats.R
@@ -1,9 +1,8 @@
 library(dplyr)  # load dplyr package for data wrangling
 
 # read csv file containing stats
+print("Loading up Tom Brady's stats...")
 tbrady <- read.csv(file="qb/tbrady.csv",header=T,sep=",")
-
-print("Tom Brady stats loaded!")
 
 print("Sorting Yards by Age/Year...")
 yards <- tbrady %>% group_by(Age) %>% select(Age,Year,Yds)
