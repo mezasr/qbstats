b304747048f1a75ad56440aa23f97c576cc03d11
steve <mezasr@gmail.com>
2017-03-27 18:29:32 -0400

    filter seasons in which qb started >= 10 games

--- read_qb_stats.R
+++ read_qb_stats.R
@@ -4,6 +4,7 @@ library(dplyr)  # load dplyr package for data wrangling
 print("Loading up Tom Brady's stats...")
 tbrady <- read.csv(file="qb/tbrady.csv",header=T,sep=",")
 
+# sort total yards by age
 print("Sorting Yards by Age/Year...")
-yards <- tbrady %>% group_by(Age) %>% select(Age,Year,Yds)
+yards <- tbrady %>% group_by(Age) %>% filter(GS>=10) %>% select(Age,Year,Yds)
 print(yards)
